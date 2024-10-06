2f89f28fc1186458e1eaaf1c6faff36b966e7c18
Reiu-arch <vidaldominguez60@gmail.com>
2024-10-06 13:42:59 -0600

    O1L2

---
+++ Labs/Objective1Lab2.java
@@ -0,0 +1,12 @@
+public class Objective1Lab2 {
+	public static void main (String [] args) {
+		System.out.println(" ----------------");
+		//one space & 16 char. long 
+		System.out.println("| Grocery List   |");
+		//17 total
+		System.out.println("| 1. Milk        |");
+		System.out.println("| 2. Eggs        |");
+		System.out.println("| 3. Bread       |");
+		System.out.println(" ----------------");
+	}
+}
