267cb0ed1eb9dacfd0295887ca2bcd284925c19e
Reiu-arch <vidaldominguez60@gmail.com>
2024-10-06 16:25:57 -0600

    O2L3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2class3 {
+	public static void main(String[] args) {
+		int birth_year = 2002;
+		String birth_month = "march";
+		boolean weekend = true;
+		System.out.println(birth_year);
+		System.out.println(birth_month);
+		System.out.println(weekend);
+	}
+}
