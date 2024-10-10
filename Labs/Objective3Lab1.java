df5aaa87f08203ded716f4723c5f0c2c6e297ddb
Reiu-arch <vidaldominguez60@gmail.com>
2024-10-10 13:52:37 -0600

    O3L1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,10 @@
+public class Objective3Lab1 {
+	public static void main(String[] args) {
+		byte num1 = 3;
+        byte num2 = 8;
+        byte num3 = 3;
+        int crossant = num1 + num2 + num3;
+        double average = (double) crossant / 3;
+        System.out.println(average);
+	}
+}
