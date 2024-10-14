80dfa4d3ae23b3a6e079a0b5fd09315274d56e42
Reiu-arch <vidaldominguez60@gmail.com>
2024-10-13 19:05:51 -0600

    for looping

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,11 @@
+public class Objective8Lab1 {
+    public static void main(String[] args) {
+        int womp = 1;
+                for (womp = 1; womp <= 10; womp+=1){
+                    System.out.println(womp);
+                }
+                for (womp = 10; womp >= 1; womp-=1){
+                    System.out.println(womp);
+                }
+    }
+}
